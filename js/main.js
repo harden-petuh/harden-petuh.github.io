aa30f18dbd2c5cc245f373cec46207270ac6936b
asd <targethunter0@gmail.com>
2018-10-05 20:03:09 +0300

    такая хуйня

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var targetDate = new Date("Jun 5, 2019 00:00:00").getTime();
+
+var x = setInterval(function () {
+  var currentDate = new Date().getTime();
+  var distance = targetDate - currentDate;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toFixed();
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.querySelector(".timer__left").innerHTML = days + " : " + hours + " : "
+    + minutes + " : " + seconds;
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 400);
